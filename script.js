057eaced32b0cc9409f20c397e0f64783c7fe23f
Ugo Anekwe <uanekwe@squiz.net>
2017-01-11 15:54:50 +1100

    Modified script to pick inputs

--- script.js
+++ script.js
@@ -3,3 +3,23 @@
 //FONT COLOR
 //ROTATE
 //IMAGE BLUR
+
+
+// GET ALL INPUT ELEMENTS
+const allInput = document.querySelectorAll(".container input");
+console.log(allInput);
+
+allInput.forEach(function(input) {
+    input.addEventListener("change", function(){
+        console.log(this);
+        const sizing = this.dataset.sizing || "";
+        document.documentElement.style.setProperty(`--${this.name}`, this.value+sizing);
+        console.log(this.name +": "+this.value +sizing);
+    });
+
+})
+
+
+//TODO: Find image to properly test for blur
+//TODO: add required styles for intended effect - color, blur
+//TODO: add comments to help practice and explain your code
